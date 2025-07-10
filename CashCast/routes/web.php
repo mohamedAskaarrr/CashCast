7404d78681798b6e24370fb8211c0370f3e3c4f9
Askarr 230104752 <mohamed230104752@sut.edu.eg>
2025-07-10 18:42:50 +0300

    Add supervisor routes with role-based access control
    Introduced new routes for supervisor resource management, including resourceful routes, a custom index route, and a permission assignment route. All supervisor-related routes are protected with 'admin' role middleware to ensure only authorized users can access them.

--- CashCast/routes/web.php
+++ CashCast/routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\UsersController;
 use App\Http\Controllers\BudgetController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\SuperVisorController;
+use Spatie\Permission\Models\Role;
 
 Route::get('/', function () {
     return redirect('/dashboard');
@@ -20,4 +22,18 @@ Route::post('/logout', [UsersController::class, 'logout'])->name('logout');
 Route::middleware('auth')->get('/dashboard', [BudgetController::class, 'dashboard']);
 
 
+
+
 Route::post('/transactions', [TransactionController::class, 'store']);
+
+
+
+
+Route::resource('supervisors', SuperVisorController::class)->middleware('role:admin');
+
+Route::get('/supervisor', [SuperVisorController::class, 'index'])->name('supervisors.superVisor');
+
+
+
+Route::post('supervisors/give-permission', [SuperVisorController::class, 'givePermission'])->middleware('role:admin');
+
